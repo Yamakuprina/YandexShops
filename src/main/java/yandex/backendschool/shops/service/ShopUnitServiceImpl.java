ce7b1e154f86e101112b8fe27dcb95481061ff6e
Tagir Faizullin <demoflava@gmail.com>
2022-07-18 20:31:56 +0300

    Update ShopUnitServiceImpl.java

--- src/main/java/yandex/backendschool/shops/service/ShopUnitServiceImpl.java
+++ src/main/java/yandex/backendschool/shops/service/ShopUnitServiceImpl.java
@@ -12,10 +12,7 @@ import javax.transaction.Transactional;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.Date;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class ShopUnitServiceImpl implements ShopUnitService {
@@ -28,15 +25,83 @@ public class ShopUnitServiceImpl implements ShopUnitService {
         this.shopUnitStatisticUnitRepository = statisticUnitRepository;
     }
 
+    private void checkConsistency(List<ShopUnit> newUnits) throws Exception {
+        for (ShopUnit unit : newUnits) {
+            if (unit.getId().length() != 36) throw new Exception("Validation Failed");
+            if (unit.getParentId() != null)
+                if (unit.getParentId().length() != 36) throw new Exception("Validation Failed");
+            if (unit.getId().equals(unit.getParentId())) throw new Exception("Validation Failed");
+            if (unit.getType().equals(ShopUnitType.CATEGORY) && unit.getPrice() != null)
+                throw new Exception("Validation Failed");
+            if (unit.getType().equals(ShopUnitType.OFFER) && (unit.getPrice() == null || unit.getPrice() < 0))
+                throw new Exception("Validation Failed");
+            if (unit.getParentId() == null) continue;
+            Optional<ShopUnit> unitParent = newUnits.stream().filter(shopUnit -> Objects.equals(unit.getParentId(), shopUnit.getId())).findFirst();
+            if (unitParent.isEmpty()) unitParent = shopUnitRepository.findById(unit.getParentId());
+            if (unitParent.isPresent() && unitParent.get().getType().equals(ShopUnitType.OFFER))
+                throw new Exception("Validation Failed");
+            if (unitParent.isPresent() && Objects.equals(unitParent.get().getParentId(), unit.getId()))
+                throw new Exception("Validation Failed");
+        }
+    }
+
     @Override
     public void importShopUnits(ShopUnitImportRequest importRequest) throws Exception {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
         Date date = sdf.parse(importRequest.getUpdateDate());
         List<ShopUnit> units = Converter.importRequestToShopUnits(importRequest, date);
         checkConsistency(units);
-        shopUnitRepository.saveAll(units);
         updateCategoriesDates(units, date);
         updateCategoriesPrices(units);
+        shopUnitRepository.saveAll(units);
+        shopUnitStatisticUnitRepository.saveAll(units.stream().map(ShopUnitStatisticUnit::new).toList());
+    }
+
+    private void updateCategoriesDates(List<ShopUnit> changedUnits, Date date) {
+        for (ShopUnit shopUnit : changedUnits.stream().toList()) {
+            Optional<ShopUnit> root = Optional.ofNullable(shopUnit);
+            while (root.get().getParentId() != null) {
+                String rootCandidateId = root.get().getParentId();
+                Optional<ShopUnit> rootCandidate = changedUnits.stream().filter(shopUnit1 -> shopUnit1.getId().equals(rootCandidateId)).findFirst();
+                if (rootCandidate.isEmpty()) rootCandidate = shopUnitRepository.findById(root.get().getParentId());
+                if (rootCandidate.isEmpty()) break;
+                else root = rootCandidate;
+                root.get().setDate(date);
+                changedUnits.add(root.get());
+            }
+        }
+    }
+
+    private void updateCategoriesPrices(List<ShopUnit> changedNodes) {
+        for (ShopUnit shopUnit : changedNodes) {
+            if (shopUnit.getType().equals(ShopUnitType.OFFER)) continue;
+            calculateCategoryPrice(changedNodes, shopUnit);
+        }
+    }
+
+    private List<Integer> calculateCategoryPrice(List<ShopUnit> changedNodes, ShopUnit shopUnitCategory) {
+        List<ShopUnit> newChildren = new ArrayList<>(changedNodes.stream().filter(shopUnit1 -> Objects.equals(shopUnit1.getParentId(), shopUnitCategory.getId())).toList());
+        shopUnitCategory.setChildren(newChildren);
+        if (shopUnitCategory.getChildren().size() == 0) {
+            shopUnitCategory.setPrice(null);
+            return List.of(0, 0);
+        }
+        float sum = 0;
+        int offer_count = 0;
+        for (ShopUnit shopUnit : shopUnitCategory.getChildren()) {
+            if (shopUnit.getType().equals(ShopUnitType.OFFER)) {
+                sum += shopUnit.getPrice();
+                offer_count += 1;
+            } else {
+                List<Integer> subcategoryRes = calculateCategoryPrice(changedNodes, shopUnit);
+                sum += subcategoryRes.get(0);
+                offer_count += subcategoryRes.get(1);
+            }
+        }
+        int res = (int) Math.floor(sum / offer_count);
+        if (res == 0 && offer_count == 0) shopUnitCategory.setPrice(null);
+        else shopUnitCategory.setPrice(res);
+        return List.of((int) sum, offer_count);
     }
 
     @Override
@@ -71,6 +136,16 @@ public class ShopUnitServiceImpl implements ShopUnitService {
         return shopUnit1;
     }
 
+    private void placeNullInOfferChildren(ShopUnit shopUnit) {
+        if (shopUnit.getType().equals(ShopUnitType.OFFER)) {
+            shopUnit.setChildren(null);
+            return;
+        }
+        for (ShopUnit shopUnit1 : shopUnit.getChildren()) {
+            placeNullInOfferChildren(shopUnit1);
+        }
+    }
+
     @Override
     public ShopUnitStatisticResponse get24hSales(String stringDate) throws Exception {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
@@ -92,98 +167,5 @@ public class ShopUnitServiceImpl implements ShopUnitService {
             throw new HttpServerErrorException(HttpStatus.NOT_FOUND, "Item not found");
         List<ShopUnitStatisticUnit> units = shopUnitStatisticUnitRepository.findAllByDateGreaterThanEqualAndDateLessThanAndUnitIdEquals(startDate, endDate, id);
         return new ShopUnitStatisticResponse(units);
-    }
-
-    private void placeNullInOfferChildren(ShopUnit shopUnit) {
-        if (shopUnit.getType().equals(ShopUnitType.OFFER)) {
-            shopUnit.setChildren(null);
-            return;
-        }
-        for (ShopUnit shopUnit1 : shopUnit.getChildren()) {
-            placeNullInOfferChildren(shopUnit1);
-        }
-    }
-
-    private void checkConsistency(List<ShopUnit> newUnits) throws Exception {
-        for (ShopUnit unit : newUnits) {
-            if (unit.getId().length() != 36) throw new Exception("Validation Failed");
-            if (unit.getParentId()!=null)
-                if (unit.getParentId().length() != 36) throw new Exception("Validation Failed");
-            if (unit.getId().equals(unit.getParentId())) throw new Exception("Validation Failed");
-            if (unit.getType().equals(ShopUnitType.CATEGORY) && unit.getPrice() != null)
-                throw new Exception("Validation Failed");
-            if (unit.getType().equals(ShopUnitType.OFFER) && (unit.getPrice() == null || unit.getPrice() < 0))
-                throw new Exception("Validation Failed");
-            if (unit.getParentId() == null) continue;
-            Optional<ShopUnit> unitParent = newUnits.stream().filter(shopUnit -> Objects.equals(unit.getParentId(), shopUnit.getId())).findFirst();
-            if (unitParent.isEmpty()) unitParent = shopUnitRepository.findById(unit.getParentId());
-            if (unitParent.isPresent() && unitParent.get().getType().equals(ShopUnitType.OFFER))
-                throw new Exception("Validation Failed");
-            if (unitParent.isPresent() && Objects.equals(unitParent.get().getParentId(), unit.getId()))
-                throw new Exception("Validation Failed");
-        }
-    }
-
-    private void updateCategoriesDates(List<ShopUnit> changedUnits, Date date) {
-        for (ShopUnit shopUnit : changedUnits) {
-            Optional<ShopUnit> root = Optional.ofNullable(shopUnit);
-            while (root.get().getParentId() != null) {
-                String rootCandidateId = root.get().getParentId();
-                Optional<ShopUnit> rootCandidate = changedUnits.stream().filter(shopUnit1 -> shopUnit1.getId().equals(rootCandidateId)).findFirst();
-                if (rootCandidate.isEmpty()) rootCandidate = shopUnitRepository.findById(root.get().getParentId());
-                if (rootCandidate.isEmpty()) break;
-                else root = rootCandidate;
-                root.get().setDate(date);
-                shopUnitRepository.save(root.get());
-                shopUnitStatisticUnitRepository.save(new ShopUnitStatisticUnit(root.get()));
-            }
-            root.get().setDate(date);
-            shopUnitRepository.save(root.get());
-            shopUnitStatisticUnitRepository.save(new ShopUnitStatisticUnit(root.get()));
-        }
-    }
-
-    private void updateCategoriesPrices(List<ShopUnit> changedNodes) {
-        for (ShopUnit shopUnit : changedNodes) {
-            Optional<ShopUnit> root = Optional.ofNullable(shopUnit);
-            shopUnitStatisticUnitRepository.save(new ShopUnitStatisticUnit(root.get()));
-            while (root.get().getParentId() != null) {
-                String rootCandidateId = root.get().getParentId();
-                Optional<ShopUnit> rootCandidate = changedNodes.stream().filter(shopUnit1 -> shopUnit1.getId().equals(rootCandidateId)).findFirst();
-                if (rootCandidate.isEmpty()) rootCandidate = shopUnitRepository.findById(root.get().getParentId());
-                if (rootCandidate.isEmpty()) break;
-                else root = rootCandidate;
-            }
-            if (root.get().getType().equals(ShopUnitType.OFFER)) continue;
-            calculateCategoryPrice(root.get());
-            shopUnitRepository.save(root.get());
-            shopUnitStatisticUnitRepository.save(new ShopUnitStatisticUnit(root.get()));
-        }
-    }
-
-
-    private List<Integer> calculateCategoryPrice(ShopUnit shopUnitCategory) {
-        shopUnitCategory.setChildren(shopUnitRepository.findByParentIdEquals(shopUnitCategory.getId()));
-        if (shopUnitCategory.getChildren().size() == 0) {
-            shopUnitCategory.setPrice(null);
-            return List.of(0, 0);
-        }
-        float sum = 0;
-        int offer_count = 0;
-        for (ShopUnit shopUnit : shopUnitCategory.getChildren()) {
-            if (shopUnit.getType().equals(ShopUnitType.OFFER)) {
-                sum += shopUnit.getPrice();
-                offer_count += 1;
-            } else {
-                List<Integer> subcategoryRes = calculateCategoryPrice(shopUnit);
-                sum += subcategoryRes.get(0);
-                offer_count += subcategoryRes.get(1);
-            }
-        }
-        int res = (int) Math.floor(sum / offer_count);
-        if (res == 0 && offer_count == 0) shopUnitCategory.setPrice(null);
-        else shopUnitCategory.setPrice(res);
-        shopUnitStatisticUnitRepository.save(new ShopUnitStatisticUnit(shopUnitCategory));
-        return List.of((int) sum, offer_count);
     }
 }
